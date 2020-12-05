f78337af1b8c6562819c46c257743aa6d1cc97b5
kavitha333 <kavitha.xworkz.com>
2020-12-05 15:10:51 +0530

    adding program

---
+++ earphones/src/com/earphones/EarPhones.java
@@ -0,0 +1,17 @@
+package com.earphones;
+
+public class EarPhones {
+	static String cName="vivo";
+	String color="White";
+	int price=6900;
+	
+	void toHearTheSong()
+	{
+		System.out.println("earphonrs can damage the ear if we used long period of time");
+	}
+	
+	void toHearTheMusic()
+	{
+		System.out.println(color);
+	}
+}
---
+++ earphones/src/com/earphones/EarPhonesUtil.java
@@ -0,0 +1,13 @@
+package com.earphones;
+
+public class EarPhonesUtil {
+	public static void main(String a[])
+	{
+	 EarPhones hear=new EarPhones();
+	 
+	 hear.toHearTheSong();
+	  hear.toHearTheMusic();
+	  System.out.println(EarPhones.cName);
+	  
+	}
+}
