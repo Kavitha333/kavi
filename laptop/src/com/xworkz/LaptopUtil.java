0f9288e72d8b94ac745e48a84b8117a585ad8aaa
kavitha333 <kavitha.xworkz.com>
2020-12-08 10:39:42 +0530

    adding laptop program

---
+++ laptop/src/com/xworkz/LaptopUtil.java
@@ -0,0 +1,13 @@
+package com.xworkz;
+
+import com.xworkz.laptop.Laptop;
+
+public class LaptopUtil {
+	
+	public static void main(String[] a) {
+		Laptop l=new Laptop();
+		l.setName("hp");
+		System.out.println("Laptop is "+ l.getName());
+	}
+
+}
---
+++ laptop/src/com/xworkz/laptop/Laptop.java
@@ -0,0 +1,15 @@
+package com.xworkz.laptop;
+//import com.xworkz.laptop.Laptop;
+
+public class Laptop {
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+
+}
