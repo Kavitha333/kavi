a269e475e508147af0774a12154575f181c895c0
kavitha333 <kavitha.xworkz.com>
2020-12-10 00:34:20 +0530

    Adding example for inharitance

---
+++ university/src/com/xworkz/Reva.java
@@ -0,0 +1,23 @@
+//package com.xworkz;
+//
+//import com.xworkz.university.University;
+//
+//public class Reva extends University {
+//	
+//
+//}
+
+
+//using constructor
+package com.xworkz;
+
+import com.xworkz.university.University;
+
+public class Reva extends University {
+	public Reva(String name,String location,long noOfUniversity) {
+		super(name,location, noOfUniversity);
+		
+	}
+	
+
+}
---
+++ university/src/com/xworkz/Vtu.java
@@ -0,0 +1,21 @@
+//package com.xworkz;
+//
+//import com.xworkz.university.University;
+//
+//public class Vtu extends University {
+//	
+//}
+
+
+//using constructor
+package com.xworkz;
+
+import com.xworkz.university.University;
+
+public class Vtu extends University {
+	public Vtu(String name,String location,long noOfUniversity) {
+		super(name,location, noOfUniversity);
+		
+	}
+
+}
