87e22ecf0591583427548ae3cc85aef625689afc
kavitha333 <kavitha.xworkz.com>
2020-12-24 01:04:20 +0530

    adding curdoperatin

---
+++ waterpark/src/com/xworkz/xworkzapp/waterpark/ManagerDTO.java
@@ -0,0 +1,21 @@
+package com.xworkz.xworkzapp.waterpark;
+
+public class ManagerDTO {
+	private String name;
+	private long phoneNo;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	public long getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(long phoneNo) {
+		this.phoneNo=phoneNo;
+	}
+
+}
